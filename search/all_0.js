bd883e73e64d643596af2cc85c0851cbfbf0dd94
icouldbreathe <icouldbreathe@users.noreply.github.com>
2024-05-14 22:44:47 +0000

    deploy: 85d0860d9ea2ea87b11eb2a7145fc9e9b2386a55

--- search/all_0.js
+++ search/all_0.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['array_0',['Array',['../classicb_1_1Array.html',1,'icb::Array&lt; T, SIZE &gt;'],['../classicb_1_1Array.html#aef8755df46497af33129e12533d1b478',1,'icb::Array::Array()=default'],['../classicb_1_1Array.html#af17210cae87e475812bec99d437757f8',1,'icb::Array::Array(std::initializer_list&lt; T &gt; &amp;&amp;list)']]],
+  ['array_0',['Array',['../classicb_1_1Array.html',1,'icb::Array&lt; T, SIZE &gt;'],['../classicb_1_1Array.html#a62aecb99af044dca0c0b3aa327365dde',1,'icb::Array::Array()']]],
   ['array_2eh_1',['array.h',['../array_8h.html',1,'']]],
   ['at_2',['At',['../classicb_1_1Vector.html#aefc70597f0f1c46e5bdeb967c6240581',1,'icb::Vector::At(SizeType index)'],['../classicb_1_1Vector.html#a98e478a1012b4e4f49a106955ec2f1d1',1,'icb::Vector::At(SizeType index) const']]]
 ];
