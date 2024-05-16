58c90023d930b4b365eb1584a30787f85a163ef0
icouldbreathe <icouldbreathe@users.noreply.github.com>
2024-05-16 19:57:08 +0000

    deploy: 32f1cdb9510f59f5efb1f2422970e82fa685f50f

--- search/all_3.js
+++ search/all_3.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['data_0',['Data',['../classicb_1_1Vector.html#a3b92b94efba0a0dc8f521a0589d6524b',1,'icb::Vector::Data()'],['../classicb_1_1Vector.html#aeac3ee1c56c0d95c4d6dcf7f4da59ccb',1,'icb::Vector::Data() const']]]
+  ['data_0',['Data',['../classicb_1_1Vector.html#a3b92b94efba0a0dc8f521a0589d6524b',1,'icb::Vector::Data()'],['../classicb_1_1Vector.html#aeac3ee1c56c0d95c4d6dcf7f4da59ccb',1,'icb::Vector::Data() const']]],
+  ['delete_1',['Delete',['../classicb_1_1AVLSet.html#acba848336bcb43626bd4ef3b363b3973',1,'icb::AVLSet']]]
 ];
